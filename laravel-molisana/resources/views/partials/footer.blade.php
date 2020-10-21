22d7179a206c29aaef542c584b56039d0c2d0846
Carlo Giuseppe Colombo <cgcolombo1983@hotmail.com>
2020-10-21 02:10:35 +0200

    final
    ok total responsive, ok footer

--- laravel-molisana/resources/views/partials/footer.blade.php
+++ laravel-molisana/resources/views/partials/footer.blade.php
@@ -1,55 +1,59 @@
 <!-- Footer -->
 <footer class="footer">
-    <div class="footer__top">
-        <div class="footer__col">
-            <div class="logo logo--footer">
-                <img class="logo__img" src="{{asset('images/marchio.png')}}" alt="logo img">
+    <div class="container">
+        <div class="footer__top">
+            <div class="footer__col">
+                <div class="logo logo--footer">
+                    <img class="logo__img" src="{{asset('images/marchio.png')}}" alt="logo img">
+                </div>
+                <ul class="footer__list footer__list--info">
+                    <li class="footer__item">Ragione Sociale: La Molisana S.P.A.</a></li>
+                    <li class="footer__item">Sede legale: Contrada Colle delle Alpi,100/A</a></li>
+                    <li class="footer__item">86100 - Campobasso</a></li>
+                    <li class="footer__item">Pec:  <a href="#" class="footer__link">lamolisana@pec.it</a></li>
+                    <li class="footer__item">Tel: <a href="#" class="footer__link">+39 0874 4981</a></li>
+                    <li class="footer__item">Fax: <a href="#" class="footer__link">+39 0874 4981</a></li>
+                    <li class="footer__item">Tel<a href="#" class="footer__link">+39 0874 629584</a></li>
+                    <li class="footer__item"><a href="#" class="footer__link">info@lamolisiana</a>(per segnalazione degli utenti)</li>
+                    <li class="footer__item"><a href="#" class="footer__link">commerciale@lamolisiana.it</a></li>
+                    <li class="footer__item"><a href="#" class="footer__link">export@lamolisiana.it</a></li>
+                    <li class="footer__item">Numero verde <a href="#" class="footer__link">800818081</a></li>
+                    <li class="footer__item">Telefono <a href="#" class="footer__link">3801292455</a></li>
+
+                </ul>
             </div>
-            <ul class="footer__list">
-                <div class="footer__item"><a href="#" class="footer__link">Il Pastificio</a></div>
-                <div class="footer__item"><a href="#" class="footer__link">Grano decorticato a pietra</a></div>
-                <div class="footer__item"><a href="#" class="footer__link">Il Molise c’è</a></div>
-                <div class="footer__item"><a href="#" class="footer__link">Filiera Integrata</a></div>
-                <div class="footer__item"><a href="#" class="footer__link">100 anni di pasta</a></div>
-                <div class="footer__item"><a href="#" class="footer__link">Sartoria della pasta</a></div>
-                <div class="footer__item"><a href="#" class="footer__link">Spaghetto Quadrato</a></div>
-                <div class="footer__item"><a href="#" class="footer__link">Le Persone</a></div>
-                <div class="footer__item"><a href="#" class="footer__link">Il Pastificio</a></div>
-                <div class="footer__item"><a href="#" class="footer__link">Grano decorticato a pietra</a></div>
-                <div class="footer__item"><a href="#" class="footer__link">Il Molise c’è</a></div>
-            </ul>
-        </div>
-        <div class="footer__col ">
-            <h3 class="footer__title">Pastificio</h3>
-            <ul class="footer__list">
-                <div class="footer__item"><a href="#" class="footer__link">Il Pastificio</a></div>
-                <div class="footer__item"><a href="#" class="footer__link">Grano decorticato a pietra</a></div>
-                <div class="footer__item"><a href="#" class="footer__link">Il Molise c’è</a></div>
-                <div class="footer__item"><a href="#" class="footer__link">Filiera Integrata</a></div>
-                <div class="footer__item"><a href="#" class="footer__link">100 anni di pasta</a></div>
-                <div class="footer__item"><a href="#" class="footer__link">Sartoria della pasta</a></div>
-                <div class="footer__item"><a href="#" class="footer__link">Spaghetto Quadrato</a></div>
-                <div class="footer__item"><a href="#" class="footer__link">Le Persone</a></div>
-            </ul>
-            <h3 class="footer__title">Prodotti</h3>
-            <ul class="footer__list">
-                <div class="footer__item"><a href="#" class="footer__link">Le Classiche</a></div>
-                <div class="footer__item"><a href="#" class="footer__link">Le Integrali</a></div>
-                <div class="footer__item"><a href="#" class="footer__link">Le Speciali</a></div>
-                <div class="footer__item"><a href="#" class="footer__link">Le Biologiche</a></div>
-                <div class="footer__item"><a href="#" class="footer__link">Le Gluten-Free</a></div>
-                <div class="footer__item"><a href="#" class="footer__link">Le Semole</a></div>
-                <div class="footer__item"><a href="#" class="footer__link">Le Extra di Lusso</a></div>
-            </ul>
-        </div>
-        <div class="footer__col">
-            <h3 class="footer__title">COLLEZIONE DA CHEF</h3>
-            <ul class="footer__list">
-            <div class="footer__item"><a href="#" class="footer__link">Collezione da Chef</a></div>
-            <div class="footer__item"><a href="#" class="footer__link">Grandi Cucine</a></div>
-            <div class="footer__item"><a href="#" class="footer__link">Biologiche</a></div>
-            <div class="footer__item"><a href="#" class="footer__link">Quadrate</a></div>
-            </ul>
+            <div class="footer__col ">
+                <h3 class="footer__title">Pastificio</h3>
+                <ul class="footer__list">
+                    <li class="footer__item"><a href="#" class="footer__link">Il Pastificio</a></li>
+                    <li class="footer__item"><a href="#" class="footer__link">Grano decorticato a pietra</a></li>
+                    <li class="footer__item"><a href="#" class="footer__link">Il Molise c’è</a></li>
+                    <li class="footer__item"><a href="#" class="footer__link">Filiera Integrata</a></li>
+                    <li class="footer__item"><a href="#" class="footer__link">100 anni di pasta</a></li>
+                    <li class="footer__item"><a href="#" class="footer__link">Sartoria della pasta</a></li>
+                    <li class="footer__item"><a href="#" class="footer__link">Spaghetto Quadrato</a></li>
+                    <li class="footer__item"><a href="#" class="footer__link">Le Persone</a></li>
+                </ul>
+                <h3 class="footer__title">Prodotti</h3>
+                <ul class="footer__list">
+                    <li class="footer__item"><a href="#" class="footer__link">Le Classiche</a></li>
+                    <li class="footer__item"><a href="#" class="footer__link">Le Integrali</a></li>
+                    <li class="footer__item"><a href="#" class="footer__link">Le Speciali</a></li>
+                    <li class="footer__item"><a href="#" class="footer__link">Le Biologiche</a></li>
+                    <li class="footer__item"><a href="#" class="footer__link">Le Gluten-Free</a></li>
+                    <li class="footer__item"><a href="#" class="footer__link">Le Semole</a></li>
+                    <li class="footer__item"><a href="#" class="footer__link">Le Extra di Lusso</a></li>
+                </ul>
+            </div>
+            <div class="footer__col">
+                <h3 class="footer__title">COLLEZIONE DA CHEF</h3>
+                <ul class="footer__list">
+                <li class="footer__item"><a href="#" class="footer__link">Collezione da Chef</a></li>
+                <li class="footer__item"><a href="#" class="footer__link">Grandi Cucine</a></li>
+                <li class="footer__item"><a href="#" class="footer__link">Biologiche</a></li>
+                <li class="footer__item"><a href="#" class="footer__link">Quadrate</a></li>
+                </ul>
+            </div>
         </div>
     </div>
     <div class="footer__bottom">
