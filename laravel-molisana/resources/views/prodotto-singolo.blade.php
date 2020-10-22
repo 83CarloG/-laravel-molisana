c25a67834ba1f4cfd341c924315e3adfd7b3a71d
Carlo Giuseppe Colombo <cgcolombo1983@hotmail.com>
2020-10-23 00:44:29 +0200

    fix

--- laravel-molisana/resources/views/prodotto-singolo.blade.php
+++ laravel-molisana/resources/views/prodotto-singolo.blade.php
@@ -3,11 +3,28 @@
     //
 @endsection
 @section('mainContent')
-    <h1>{{$data['titolo']}}</h1>
-    <img src="{{$data['src-h']}}" alt="#">
-    <img src="{{$data['src-p']}}" alt="#">
-    <p>
-        {!!$data['descrizione']!!}
-    </p>
+<div class="main-prodotti">
+    <div class="container">
+        <h1 class="main-prodotti__title">{{$data['titolo']}}</h1>
+        <div class="main-prodotti__figure">
+            <img class="main-prodotti__image" src="{{$data['src-h']}}" alt="#">
+        </div>
+        <div class="main-prodotti__figure">
+            <img class="main-prodotti__image" src="{{$data['src-p']}}" alt="#">
+        </div>
+
+
+
+        <p class="main-prodotti__text">
+            {!!$data['descrizione']!!}
+        </p>
+    <div>
+       <a href="/prodotti/show/"><i class="main-prodotti__icon fas fa-angle-left"></i></a>
+    </div>
+    <div>
+        <a href="/prodotti/show/"><i class="main-prodotti__icon fas fa-angle-right"></i></a>
+    </div>
+    </div>
+</div>
 
 @endsection
